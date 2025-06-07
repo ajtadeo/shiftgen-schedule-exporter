1b7eec09c20a47906dae63ea3b0e2c243caef095
Alyssa Tadeo <alyssajtadeo@gmail.com>
2025-06-07 13:49:53 -0700

    rename src/shiftgen_calendar to src/shiftgen, update debug shift info to use new format

--- src/global.js
+++ src/global.js
@@ -24,92 +24,92 @@ const SITE_ID_ENUM = {
 }
 
 const TEST_SHIFTS = {
-  "2025-03-09T15:00:00.000-08:00": {
-      "endDateTime": "2025-03-09T23:30:00.000-08:00",
-      "location": "PA",
-      "overnight": true,
-      "providerName": "NISHIOKA",
-      "providerType": 2,
-      "startDateTime": "2025-03-09T15:00:00.000-08:00"
-  },
-  "2025-03-10T12:30:00.000-08:00": {
-      "endDateTime": "2025-03-10T21:00:00.000-08:00",
-      "location": "South",
-      "overnight": false,
-      "providerName": "SAINTGEORGES",
-      "providerType": 1,
-      "startDateTime": "2025-03-10T12:30:00.000-08:00"
-  },
-  "2025-03-11T16:00:00.000-08:00": {
-      "endDateTime": "2025-03-12T00:00:00.000-08:00",
-      "location": "RED",
-      "overnight": true,
-      "providerName": "ASSAF",
-      "providerType": 1,
-      "startDateTime": "2025-03-11T16:00:00.000-08:00"
-  },
-  "2025-03-12T16:00:00.000-08:00": {
-      "endDateTime": "2025-03-13T00:00:00.000-08:00",
-      "location": "RED",
-      "overnight": true,
-      "providerName": "ROGAN",
-      "providerType": 1,
-      "startDateTime": "2025-03-12T16:00:00.000-08:00"
-  },
-  "2025-03-17T16:00:00.000-08:00": {
-      "endDateTime": "2025-03-18T00:00:00.000-08:00",
-      "location": "RED",
-      "overnight": true,
-      "providerName": "SAINTGEORGES",
-      "providerType": 1,
-      "startDateTime": "2025-03-17T16:00:00.000-08:00"
-  },
-  "2025-03-18T16:00:00.000-08:00": {
-      "endDateTime": "2025-03-19T00:00:00.000-08:00",
-      "location": "RED",
-      "overnight": true,
-      "providerName": "ENGLAND",
-      "providerType": 1,
-      "startDateTime": "2025-03-18T16:00:00.000-08:00"
-  },
-  "2025-03-19T15:00:00.000-08:00": {
-      "endDateTime": "2025-03-19T23:30:00.000-08:00",
-      "location": "PA",
-      "overnight": true,
-      "providerName": "JIVAN",
-      "providerType": 2,
-      "startDateTime": "2025-03-19T15:00:00.000-08:00"
-  },
-  "2025-03-24T16:00:00.000-08:00": {
-      "endDateTime": "2025-03-25T00:00:00.000-08:00",
-      "location": "RED",
-      "overnight": true,
-      "providerName": "DICKSON",
-      "providerType": 1,
-      "startDateTime": "2025-03-24T16:00:00.000-08:00"
-  },
-  "2025-03-25T19:30:00.000-08:00": {
-      "endDateTime": "2025-03-26T04:00:00.000-08:00",
-      "location": "PA",
-      "overnight": true,
-      "providerName": "FIX ME",
-      "providerType": -1,
-      "startDateTime": "2025-03-25T19:30:00.000-08:00"
-  },
-  "2025-03-26T20:30:00.000-08:00": {
-      "endDateTime": "2025-03-27T05:00:00.000-08:00",
-      "location": "South",
-      "overnight": true,
-      "providerName": "JAYAMAHA",
-      "providerType": 1,
-      "startDateTime": "2025-03-26T20:30:00.000-08:00"
-  },
-  "2025-03-30T20:30:00.000-08:00": {
-      "endDateTime": "2025-03-31T05:00:00.000-08:00",
-      "location": "North",
-      "overnight": true,
-      "providerName": "JAYAMAHA",
-      "providerType": 1,
-      "startDateTime": "2025-03-30T20:30:00.000-08:00"
-  }
+    '1741561200000': {
+        endTime: 1741591800000,
+        location: 'PA',
+        overnight: true,
+        providerName: 'NISHIOKA',
+        providerType: 2,
+        startTime: 1741561200000
+    },
+    '1741638600000': {
+        endTime: 1741669200000,
+        location: 'South',
+        overnight: false,
+        providerName: 'SAINTGEORGES',
+        providerType: 1,
+        startTime: 1741638600000
+    },
+    '1741737600000': {
+        endTime: 1741766400000,
+        location: 'RED',
+        overnight: true,
+        providerName: 'ASSAF',
+        providerType: 1,
+        startTime: 1741737600000
+    },
+    '1741824000000': {
+        endTime: 1741852800000,
+        location: 'RED',
+        overnight: true,
+        providerName: 'ROGAN',
+        providerType: 1,
+        startTime: 1741824000000
+    },
+    '1742256000000': {
+        endTime: 1742284800000,
+        location: 'RED',
+        overnight: true,
+        providerName: 'SAINTGEORGES',
+        providerType: 1,
+        startTime: 1742256000000
+    },
+    '1742342400000': {
+        endTime: 1742371200000,
+        location: 'RED',
+        overnight: true,
+        providerName: 'ENGLAND',
+        providerType: 1,
+        startTime: 1742342400000
+    },
+    '1742425200000': {
+        endTime: 1742455800000,
+        location: 'PA',
+        overnight: true,
+        providerName: 'JIVAN',
+        providerType: 2,
+        startTime: 1742425200000
+    },
+    '1742860800000': {
+        endTime: 1742889600000,
+        location: 'RED',
+        overnight: true,
+        providerName: 'DICKSON',
+        providerType: 1,
+        startTime: 1742860800000
+    },
+    '1742959800000': {
+        endTime: 1742990400000,
+        location: 'PA',
+        overnight: true,
+        providerName: 'JIVAN',
+        providerType: 2,
+        startTime: 1742959800000
+    },
+    '1743049800000': {
+        endTime: 1743080400000,
+        location: 'South',
+        overnight: true,
+        providerName: 'JAYAMAHA',
+        providerType: 1,
+        startTime: 1743049800000
+    },
+    '1743395400000': {
+        endTime: 1743426000000,
+        location: 'North',
+        overnight: true,
+        providerName: 'JAYAMAHA',
+        providerType: 1,
+        startTime: 1743395400000
+    }
 }
